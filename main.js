ee67fd4ac6dca723557000d56045a0147ed9feac
Quinn Shannon <quinn.p.shannon@gmail.com>
2026-05-14 12:47:31 -0400

    Grabbed all the Pokemon Data from the API, and transformed it with Express to keep it simple. Thanks Express!

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import {types, typeChart} from "./pokemonTypes.js"
+import {pokemon} from "./pokemonSpecies.js"
+import {trainers} from "./pokemonTrainers.js"
 
 typeChart()
--- pokemonTrainers.js
+++ pokemonTrainers.js
@@ -11,7 +11,18 @@
             }
     }
 */
-const trainers = [
+export const trainers = [
+    {
+        id: 0,
+        name: "Blue",
+        pokemon: [
+            {
+                id: 133,
+                nickname: "",
+                level: 5
+            },
+        ]
+    },
     {
         id: 1,
         name: "Brock",
@@ -25,6 +36,173 @@ const trainers = [
                 id: 95,
                 nickname: "",
                 level: 14
+            },
+        ]
+    },
+    {
+        id: 2,
+        name: "Misty",
+        pokemon: [
+            {
+                id: 120,
+                nickname: "",
+                level: 18
+            },
+            {
+                id: 121,
+                nickname: "",
+                level: 21
+            },
+        ]
+    },
+    {
+        id: 3,
+        name: "Lt. Surge",
+        pokemon: [
+            {
+                id: 100,
+                nickname: "",
+                level: 21
+            },
+            {
+                id: 25,
+                nickname: "",
+                level: 18
+            },
+            {
+                id: 26,
+                nickname: "",
+                level: 24
+            },
+        ]
+    },
+    {
+        id: 4,
+        name: "Erika",
+        pokemon: [
+            {
+                id: 71,
+                nickname: "",
+                level: 29
+            },
+            {
+                id: 114,
+                nickname: "",
+                level: 24
+            },
+            {
+                id: 45,
+                nickname: "",
+                level: 29
+            },
+        ]
+    },
+    {
+        id: 5,
+        name: "Koga",
+        pokemon: [
+            {
+                id: 109,
+                nickname: "",
+                level: 37
+            },
+            {
+                id: 89,
+                nickname: "",
+                level: 39
+            },
+            {
+                id: 109,
+                nickname: "",
+                level: 37
+            },
+            {
+                id: 110,
+                nickname: "",
+                level: 43
+            },
+        ]
+    },
+    {
+        id: 6,
+        name: "Sabrina",
+        pokemon: [
+            {
+                id: 64,
+                nickname: "",
+                level: 38
+            },
+            {
+                id: 122,
+                nickname: "",
+                level: 37
+            },
+            {
+                id: 49,
+                nickname: "",
+                level: 38
+            },
+            {
+                id: 65,
+                nickname: "",
+                level: 43
+            },
+        ]
+    },
+    {
+        id: 7,
+        name: "Blaine",
+        pokemon: [
+            {
+                id: 58,
+                nickname: "",
+                level: 42
+            },
+            {
+                id: 77,
+                nickname: "",
+                level: 40
+            },
+            {
+                id: 78,
+                nickname: "",
+                level: 42
+            },
+            {
+                id: 59,
+                nickname: "",
+                level: 47
+            },
+        ]
+    },
+    {
+        id: 8,
+        name: "Giovanni",
+        pokemon: [
+            {
+                id: 111,
+                nickname: "",
+                level: 45
+            },
+            {
+                id: 51,
+                nickname: "",
+                level: 42
+            },
+            {
+                id: 31,
+                nickname: "",
+                level: 44
+            },
+            {
+                id: 34,
+                nickname: "",
+                level: 45
+            },
+            {
+                id: 112,
+                nickname: "",
+                level: 50
             },
         ]
     },
