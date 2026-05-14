bc9775ea1091b302e96db687471f8e431be5f6be
Quinn Shannon <quinn.p.shannon@gmail.com>
2026-05-14 17:18:10 -0400

    Added Problems to main.js

--- main.js
+++ main.js
@@ -3,3 +3,32 @@ import {pokemon} from "./pokemonSpecies.js"
 import {trainers} from "./pokemonTrainers.js"
 
 typeChart()
+
+/* 
+    Problem 1
+    Write a function to find the name of a Pokemon, given the id
+*/
+
+/*
+    Problem 2
+    Write a function that, given a trainer, will list all of the Trainer's information!
+    Also display the Average Level of their Pokemon.
+*/
+
+/*
+    Problem 3
+    Write a function to add 2 properties to the Pokemon Object:
+    superEffective {
+        [type]: multiplier,
+    }
+    notVeryEffective {
+        [type]: multiplier,
+    }
+    Only add the types to their respective object IF they meet the effectiveness criteria!
+*/
+
+/*
+    Problem 4
+    Write a function to determine What pokemon type would be the Most Effective to bring
+    against a specific trainer!
+*/
